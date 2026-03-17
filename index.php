b93fd3b1d3f7fc8ea715afb0a52eb3599221c808
Eva-Hern4ndez <26000519@galileo.edu>
2026-03-17 03:21:36 +0000

    código e imagen

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Hola Mundo en PHP</title>
+</head>
+<body>
+
+<?php
+$nombre = "Eva María Hernández Macajola";
+echo "<h1>Hola mundo, soy $nombre</h1>";
+?>
+
+<br>
+
+<img src="image_IA.jpg" alt="Imagen creada con IA" width="400">
+
+</body>
+</html>
